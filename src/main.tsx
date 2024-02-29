16094dfa7fd27d3efaac3c58178fd446816e55c5
george-pench <penchev.georgii@gmail.com>
2024-02-29 18:02:42 +0200

    Add images, game, platform and company interfaces, get rid of unnecessary code in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,15 @@
 import "./styles/main.scss";
-// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-
-import React, { Component /* , StrictMode */ } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app";
-import ErrorBoundary from "./components/Error/errorBoundary";
-import ErrorPage from "./components/Error/errorPage";
-
-interface Props {}
-interface State {}
-
-class AppContainer extends Component<Props, State> {
-  render() {
-    return <App />;
-  }
-}
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import routes from "./components/router";
 
 const rootElement = document.getElementById("app");
 
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement!);
 
-  root.render(
-    <React.StrictMode>
-      <ErrorBoundary fallback={<ErrorPage />}>
-        <AppContainer />
-      </ErrorBoundary>
-    </React.StrictMode>,
-  );
-}
-
-// React + TS: https://github.com/typescript-cheatsheets/react#reacttypescript-cheatsheets
+root.render(
+  <StrictMode>
+    <RouterProvider router={routes} />
+  </StrictMode>,
+);
